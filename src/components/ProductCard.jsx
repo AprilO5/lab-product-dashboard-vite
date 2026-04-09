b84781d75f2ecea81d22cfc36467613e745cb262
April Lindie <april@MacBookPro.lan>
2026-04-09 10:49:31 -0400

    Finished product dashboard lab

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import styles from '../styles/ProductCard.module.css';
+import Button from '@mui/material/Button';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, onRemove }) => {
   return (
-    <div className>
-      {/* TODO: Apply conditional class to <div> above for out-of-stock items */}
-      
-      {/* TODO: Display product name */}
-
-      {/* TODO: Display product price */}
-
-      {/* TODO: Show if the product is in stock or out of stock */}
-      
+    <div className={`${styles.card} ${!product.inStock ? 'outOfStockClass' : ''}`}>
+      <h2>{product.name}</h2>
+      <p>{product.price}</p>
+      <p>{product.inStock ? 'In Stock' : 'Out of Stock'}</p>
+      <Button variant="contained" onClick={() => onRemove(product.id)}>
+        Remove
+      </Button>
     </div>
   );
 };
